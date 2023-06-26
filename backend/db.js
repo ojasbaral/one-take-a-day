e233ecf9274355abd8bdde0a27d0546ccafa5dbb
Ojas Baral <ojas.baral@gmail.com>
2023-06-25 23:14:49 -0600

    last attempt

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,12 @@
 const Pool = require('pg').Pool 
 
 const pool = new Pool({
-    user: process.env.DB_USER,
+    /* user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    database: process.env.DB_DB  
+    database: process.env.DB_DB   */
+    connectionString: process.env.DB_URL + "?ssl=true"
 })
 
 pool.connect((err) => {
