185821df9ffce11bdce8aa82749b7664d5f99b52
Ojas Baral <ojas.baral@gmail.com>
2023-06-25 22:56:34 -0600

    database

--- backend/db.js
+++ backend/db.js
@@ -2,11 +2,12 @@ const Pool = require('pg').Pool
 require('dotenv').config()
 
 const pool = new Pool({
-    user: process.env.DB_USER,
+    /* user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    database: process.env.DB_DB
+    database: process.env.DB_DB */
+    connectionString: process.env.DB_STRING
 })
 
 module.exports = pool
